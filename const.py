468010041da46f978120500863b03761eaab909a
avannaa <desudesu_desu@yahoo.com>
2022-11-24 12:11:58 -0300

    forgot this one

---
+++ const.py
@@ -0,0 +1,29 @@
+PB = 0.2167
+S4_WIDEFIX = 1.0396
+WINDHILL = 420  # constant that represents wind hill's ? wind
+N_CAL = 361  # total number of calipers, n=361 for 0-100%.  deprecated: n=73 for only 80-100%, n=109 for 70-100%
+PBA_PLUS = 1.024
+
+COURSE_MAP = [
+'Blue Lagoon', # 0
+'Blue Water', # 1
+'Sepia Wind', # 2
+'Wind Hill', # 3
+'Wiz Wiz', # 4
+'West Wiz', # 5
+'Blue Moon', # 6
+'Silvia Cannon', # 7
+'Ice Cannon', # 8
+'White Wiz', # 9
+'Shining Sand', # 10
+'Pink Wind', # 11
+'Eastern Valley', # 12
+'Deep Inferno', # 13
+'Ice Spa', # 14
+'Lost Seaway', # 15
+'Eastern Valley', # 16
+'???', # 17
+'Ice Inferno', # 18
+'Wiz City', # 19
+'Abbot Mine', # 20
+]
